070d339c921ffccc90f16c6264781c7c562e05e9
krkyk <madeinpink1111@gmail.com>
2022-06-16 13:10:41 +0000

    [Add]結果ページ

--- app/controllers/omikujis_controller.rb
+++ app/controllers/omikujis_controller.rb
@@ -1,4 +1,8 @@
 class OmikujisController < ApplicationController
   def show
+    @result = Omikuji.results.keys.pluck("RANDOM()").sample
+    byebug
+    @item = Omikuji.items.keys.pluck("RANDOM()").first
+    # @color = Omikuji.colors.pluck("RANDOM()").first
   end
 end
