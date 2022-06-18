b944723b5f5dfa8becb77dbfafc3ba0d2a344374
krkyk <madeinpink1111@gmail.com>
2022-06-18 12:33:48 +0000

    [Update]結果ページ

--- app/controllers/omikujis_controller.rb
+++ app/controllers/omikujis_controller.rb
@@ -1,7 +1,6 @@
 class OmikujisController < ApplicationController
   def show
     @result = Omikuji.results.keys.sample
-    #byebug
     @item = Omikuji.items.keys.sample
     @color = Omikuji.colors.keys.sample
   end
